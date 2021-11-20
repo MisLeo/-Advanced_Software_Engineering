72729762598c0bfb17855177181067aa11d48467
MisLeo <missel-leonie@gmx.de>
2021-11-20 15:24:35 +0100

    Tabelle nur mit einer Spalte, funktioniert jedoch

---
+++ Programme/Tabelle_V2/app/src/main/java/com/example/tabelle_v2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.tabelle_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView list = (ListView) findViewById(R.id.theList);
+        Log.d(TAG, "onCreate: Started.");
+
+        //Daten für Einspaltige Liste
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Mitch");
+        names.add("Blake");
+        names.add("Shelly");
+        names.add("Jess");
+        names.add("Steve");
+        names.add("Mohammed");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_item_layout, names);
+        list.setAdapter(adapter);
+
+    }
+}
